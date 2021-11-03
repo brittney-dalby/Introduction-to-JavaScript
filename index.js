3046c26c10b31a2b081aaeb973b690a601a8b921
Britt Dalby <91482954+brittney-dalby@users.noreply.github.com>
2021-11-02 23:18:35 -0600

    finished task 5

--- index.js
+++ index.js
@@ -203,10 +203,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371
 }
-
+console.log('task 5a',miles(5));
 
 
 //Task 5b - Feet to CM
@@ -217,9 +217,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm * 0.032808399
 }
+console.log('task 5b',feet(100));
 
 
 
